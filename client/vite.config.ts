5abb1da1cada6bf074b2f3243118f13221de3f41
nocom <nocomdev@bk.ru>
2026-01-30 18:52:48 +0400

    Remove Tailwind CSS from Vite configuration and adjust plugin structure

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
 import { viteSingleFile } from 'vite-plugin-singlefile'
-import tailwindcss from 'tailwindcss'
 import path from 'path'
 
 // https://vite.dev/config/
@@ -11,16 +10,16 @@ export default defineConfig({
   envPrefix: ['VITE_', 'TRANSPORTS'],
   plugins: [
     react(),
-    tailwindcss(), 
     viteSingleFile(),
     legacy({
       targets: ['defaults', 'not IE 11', 'Android >= 7'],
     }),
-     alias: {
+  ],
+  resolve: {
+    alias: {
       "@": path.resolve(__dirname, "src"),
     },
-  ],
-  server: {
+  },
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
